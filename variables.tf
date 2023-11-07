c64899c53f99ec4a6a18c3a77c5d2f09ed36242e
amar <amarmudiraj@gmail.com>
2023-11-07 22:24:26 +0530

    changes in the bucket name

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "buket_name" {
   description = "Name of the Bucket created with the variable value"
-  default     = "my-root-2023-bucket-by-tf-001"
+  default     = "demobucket-2023-bucket-by-tf-001"
 }
