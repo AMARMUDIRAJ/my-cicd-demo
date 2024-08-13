3983a057c7e922166f07d66d7bb736577e451f61
amar mudiraj <amarmudiraj@gmail.com>
2024-08-13 11:55:11 +0530

    update the bucket name var decleared

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 
-resource "google_storage_bucket" "my-first-bucket-tf" {
-  name     = var.buket_name1
+resource "google_storage_bucket" "my-bucket-tf" {
+  name     = var.buket_name
   project  = "my-dev-project-399904"
   location = "US"
+  // force_destroy = true
 }
