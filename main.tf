cc15c9b0beb37f9d3258cec815cca24de9bb89d0
amar mudiraj <amarmudiraj@gmail.com>
2024-08-13 12:51:35 +0530

    u[datew tih changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 resource "google_storage_bucket" "my-bucket-tf" {
   project  = "my-dev-project-399904"
-  name = "my-demo-testing-bucket-aug-2024-13th-001"
+  name     = "my-demo-testing-bucket-aug-2024-13th-001"
   location = "US"
   // force_destroy = true
 }
