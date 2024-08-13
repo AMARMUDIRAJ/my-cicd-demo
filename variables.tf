bdc7f77b59957968fc947da0824fe2834468b954
amar mudiraj <amarmudiraj@gmail.com>
2024-08-13 12:02:44 +0530

    Update

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "buket_name" {
   description = "Name of the Bucket created with the variable value"
-  default     = "demobucket-2023-aug-tf-001"
+  default     = "demobucket-2023-aug-for-testing-tf-001"
   // type        = string
 
 }
