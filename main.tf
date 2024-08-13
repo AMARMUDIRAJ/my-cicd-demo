8de9ca557d2dad85991147770fffa06b7f1175c5
amar mudiraj <amarmudiraj@gmail.com>
2024-08-13 11:48:38 +0530

    Updated with new change

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
-resource "google_storage_bucket" "my-first-bucket" {
-  name     = var.buket_name
+resource "google_storage_bucket" "my-first-bucket-tf" {
+  name     = var.buket_name1
   project  = "my-dev-project-399904"
   location = "US"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "buket_name" {
   description = "Name of the Bucket created with the variable value"
-  default     = "demobucket-2023-bucket-by-tf-001"
+  default     = "demobucket-2023-tf-001"
 }
