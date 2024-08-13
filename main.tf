8ae6a7eb20ababf173f403c2c4a87f561e4b4299
amar mudiraj <amarmudiraj@gmail.com>
2024-08-13 11:58:47 +0530

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "google_storage_bucket" "my-bucket-tf" {
+resource "google_storage_bucket" "my-bucket-tf1" {
   name     = var.buket_name
   project  = "my-dev-project-399904"
   location = "US"
