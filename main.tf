2f19d07b226c0d0cb92c423c53e2d304125469ac
amar mudiraj <amarmudiraj@gmail.com>
2024-08-13 16:26:11 +0530

    created var file

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 
 resource "google_storage_bucket" "my-bucket-tf" {
-  project  = "my-dev-project-399904"
-  name     = "my-demo-testing-bucket-aug-2024-13th-001"
-  location = "US"
+  project  = var.project
+  name     = var.name
+  location = var.location
   // force_destroy = true
 }
 
